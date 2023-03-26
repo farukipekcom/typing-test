e173b2a5ee404c3f933f40763a332238d8f6aeb3
Faruk İpek <farukipekweb@gmail.com>
2023-03-26 00:48:14 -0400

    added word list

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,32 @@
 export default function Home() {
-  return <></>;
+  const words = [
+    "dog",
+    "wall",
+    "powerbank",
+    "teacher",
+    "dentist",
+    "remember",
+    "sugar",
+    "camera",
+    "plant",
+    "suitcase",
+    "heater",
+  ];
+  return (
+    <>
+      <div className="main">
+        <div className="list">
+          <div className="background">
+            <ul className="words">
+              {words.map((item, id) => (
+                <li className="word" key={id}>
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
